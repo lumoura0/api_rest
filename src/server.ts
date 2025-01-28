2f37f1ceace7fd450180f9fcee0a6846f638672a
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-28 15:23:37 -0300

    configurando o eslint

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 // GET, POST, PUT, PATCH, DELETE
 app.get('/hello', () => {
-    return 'Hello'
+  return 'Hello'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('Server is running on port 3333')
-})
+  })
