5a859397d78fc0d1870322d1802636c9c5c469ee
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-28 16:29:45 -0300

    Realizando queries com knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 // GET, POST, PUT, PATCH, DELETE
+// ('transactions')
+//     .insert({
+//       id: crypto.randomUUID(),
+//       title: 'Transação de teste',
+//       amount: 1000,
+//     })
+//     .returning('*')
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
