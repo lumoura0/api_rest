686a49d65378b9e93854fc15477ac291c669c5ef
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-28 15:04:47 -0300

    Criando estrutura da aplicação

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+// GET, POST, PUT, PATCH, DELETE
+app.get('/hello', () => {
+    return 'Hello'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Server is running on port 3333')
+})
