ca0edc08e50776b734665fef68a775a833606a86
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-30 15:05:03 -0300

    Testes automatizados - Testando listagem de transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -14,7 +14,7 @@ describe('Transactions routes', () => {
   // Deve ser possivel fazer x
   it('should be able to create a new transaction', async () => {
     // Fazer a chamda HTTP p/ criar uma nova transação
-    await request(app.server)
+    const response = await request(app.server)
       .post('/transactions')
       .send({
         title: 'New transaction',
@@ -22,5 +22,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
